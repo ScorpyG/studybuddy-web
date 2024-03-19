6e094524c111b283ad399f64139519daf337ac80
lerasaltykova <lerasaltykovaa@gmail.com>
2024-03-19 12:19:14 -0700

    footer is sticky now

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -5,7 +5,9 @@ export default {
 </script>
 
 <template>
-  <footer class="text-white bg-gray-500 py-4 mt-3">
+  <footer
+    class="text-white bg-gray-500 py-4 mt-3 fixed bottom-0 left-0 z-20 w-full"
+  >
     <div class="container mx-auto px-4">
       <p class="text-center text-sm">
         &copy; 2024 StudyBuddy. All Rights Reserved.
